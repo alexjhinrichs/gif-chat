2ec21c7e7a6c84ea4ae930abd57059db7afb05d8
Alex Hinrichs <alexjhinrichs@gmail.com>
2015-10-17 22:56:11 -0600

    Done

--- js/mainControl.js
+++ js/mainControl.js
@@ -8,6 +8,7 @@ app.controller('mainController', function($scope, mainService) {
 		mainService.searchGif($scope.message)
 		.success(function(response) {
 			var randNum = Math.floor(Math.random() * response.data.length);
+			console.log(response);
 			$scope.gifs.unshift(response.data[randNum].images.fixed_width.url);
 			$scope.message = "";
 		})
